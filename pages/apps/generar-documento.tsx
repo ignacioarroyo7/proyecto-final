cd3f22571072b083385d0e0050057d49155b1514
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2022-11-07 21:47:35 -0300

    se agregaron validaciones a los formularios

--- pages/apps/generar-documento.tsx
+++ pages/apps/generar-documento.tsx
@@ -3,6 +3,14 @@ import Link from "next/link";
 import Head from "next/head";
 import Navbar from "../../components/navbar/navbar";
 import {
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  Form,
+  Field,
+  FieldProps,
+} from "formik";
+import {
   Flex,
   Box,
   FormControl,
@@ -14,61 +22,162 @@ import {
   Text,
   useColorModeValue,
   Select,
-} from '@chakra-ui/react';
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  FormErrorMessage,
+  Container,
+} from "@chakra-ui/react";
 
 const GenerarDocumento: React.FC = () => {
+    function validateLegajo(value) {
+      let error;
+      if (!value) {
+        error = "Este campo es obligatorio";
+      } else if (value.length>5) {
+        error = "Ingrese un legajo válido";
+      }
+      return error;
+    }
+    function validatePassword(value) {
+      let error;
+      if (!value) {
+        error = "Este campo es obligatorio";
+      } 
+      // else if (value.length<16) {
+      //   error = "Ingrese un hash válido";
+      // }
+      return error;
+    }
+    function validateConstancia(value) {
+      let error;
+      // if (!value) {
+      //   error = "Este campo es obligatorio";
+      // }
+      if (value=="Elija una Constancia.."||value=="") {
+        error = "Este campo es obligatorio";
+      }
+      return error;
+    }
+
+  const constancias = ['Constancia de Alumno Regular','Constancia de Inicio de Año Lectivo','Estado Académico']
   return (
     <>
-    <Navbar></Navbar>
-      <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={14} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Generar Documento</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            Ingrese los siguientes datos para generar el documento
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="legajo">
-              <FormLabel>Legajo</FormLabel>
-              <Input type="number" placeholder='Ingrese su legajo' />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Contraseña</FormLabel>
-              <Input type="password" placeholder='Ingrese su contraseña'/>
-            </FormControl>
-            <FormControl id="hash">
-            <FormLabel>Constancias</FormLabel>
-  <Select>
-  <option selected hidden disabled value="">Elija una Constancia</option>
-    <option>Constancia de Alumno Regular</option>
-    <option>Constancia de Inicio de Año Lectivo</option>
-    <option>Estado Académico</option>
-  </Select>
-            </FormControl>
-            <Stack spacing={10}>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Solcitar
-              </Button>
+      <Navbar></Navbar>
+      <Container maxW={'4xl'}>
+      <Formik
+        initialValues={{ legajo: "", password:""}}
+        onSubmit={(values, actions) => {
+          console.log('values: ',values)
+          actions.setSubmitting(false);
+        }}
+      >
+        {(props) => (
+          <Flex
+            minH={"100vh"}
+            align={"center"}
+            justify={"center"}
+            bg={useColorModeValue("gray.50", "gray.800")}
+          >
+            <Stack spacing={14} mx={"auto"} maxW={"lg"} py={12} px={6}>
+              <Stack align={"center"}>
+                <Heading fontSize={"4xl"}>Generar Documento</Heading>
+                <Text fontSize={"lg"} color={"gray.600"}>
+                Ingrese los siguientes datos para generar el documento
+                </Text>
+              </Stack>
+              <Box
+                rounded={"lg"}
+                bg={useColorModeValue("white", "gray.700")}
+                boxShadow={"lg"}
+                p={8}
+              >
+                <Stack spacing={4}>
+                  <Form>
+                  <Field name="legajo" validate={validateLegajo}>
+                      {({ field, form }) => (
+                        <FormControl
+                        isRequired={true}
+                          isInvalid={form.errors.legajo && form.touched.legajo}
+                        >
+                          <FormLabel>Legajo</FormLabel>
+                          <Input
+                            {...field}
+                            placeholder="Ingrese su legajo..."
+                            type="number"
+                            maxLength={5}
+                          />
+                          <FormErrorMessage>
+                            {form.errors.legajo}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="password" validate={validatePassword}>
+                      {({ field, form }) => (
+                        <FormControl
+                        isRequired={true}
+                          isInvalid={form.errors.password && form.touched.password}
+                        >
+                          <FormLabel>Constraseña</FormLabel>
+                          <Input
+                            {...field}
+                            placeholder="Ingrese su contraseña..."
+                            type="password"
+                            maxLength={16}
+                          />
+                          <FormErrorMessage>
+                            {form.errors.password}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="constancia" validate={validateConstancia}>
+                      {({ field, form }) => (
+                        <FormControl
+                        isRequired={true}
+                          isInvalid={form.errors.constancia && form.touched.constancia}
+                        >
+                          <FormLabel>Constancia</FormLabel>
+                          <Select
+                          {...field}
+                          >
+                  <option selected hidden disabled value="">
+                    Elija una Constancia..
+                  </option>
+                  {Object.keys(constancias).map((keyName, i) => (
+                      <option key={i} value={constancias[keyName]}>
+                        {constancias[keyName]}
+                      </option>
+                    ))}
+                </Select>
+                          <FormErrorMessage>
+                            {form.errors.constancia}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Button
+                      mt={4}
+                      bg={"blue.400"}
+                      color={"white"}
+                      _hover={{
+                        bg: "blue.500",
+                      }}
+                      // colorScheme='teal'
+                      isLoading={props.isSubmitting}
+                      type="submit"
+                    >
+                      Solicitar
+                    </Button>
+                  </Form>
+                </Stack>
+              </Box>
             </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+          </Flex>
+        )}
+      </Formik>
+      </Container>
     </>
   );
 };
--- pages/apps/validar-documento.tsx
+++ pages/apps/validar-documento.tsx
@@ -3,6 +3,14 @@ import Link from "next/link";
 import Head from "next/head";
 import Navbar from "../../components/navbar/navbar";
 import {
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  Form,
+  Field,
+  FieldProps,
+} from "formik";
+import {
   Flex,
   Box,
   FormControl,
@@ -13,48 +21,93 @@ import {
   Heading,
   Text,
   useColorModeValue,
-} from '@chakra-ui/react';
+  FormErrorMessage,
+} from "@chakra-ui/react";
 
 const ValidarDocumento: React.FC = () => {
+  function validateHash(value) {
+    let error;
+    if (!value) {
+      error = "Este campo es obligatorio";
+    } else if (value.length<32) {
+      error = "Ingrese un hash válido";
+    }
+    return error;
+  }
+
   return (
     <>
-    <Navbar></Navbar>
-      <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={14} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Verificar validez</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            Ingrese el hash para realizar la busqueda del documento
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="hash">
-              <FormLabel>Hash</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Buscar
-              </Button>
+      <Navbar></Navbar>
+      <Formik
+        initialValues={{ hash: "" }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }, 1000);
+        }}
+      >
+        {(props) => (
+          <Flex
+            minH={"100vh"}
+            align={"center"}
+            justify={"center"}
+            bg={useColorModeValue("gray.50", "gray.800")}
+          >
+            <Stack spacing={14} mx={"auto"} maxW={"lg"} py={12} px={6}>
+              <Stack align={"center"}>
+                <Heading fontSize={"4xl"}>Verificar validez</Heading>
+                <Text fontSize={"lg"} color={"gray.600"}>
+                  Ingrese el hash para realizar la busqueda del documento
+                </Text>
+              </Stack>
+              <Box
+                rounded={"lg"}
+                bg={useColorModeValue("white", "gray.700")}
+                boxShadow={"lg"}
+                p={8}
+              >
+                <Stack spacing={4}>
+                  <Form>
+                    <Field name="hash" validate={validateHash}>
+                      {({ field, form }) => (
+                        <FormControl
+                        isRequired={true}
+                          isInvalid={form.errors.hash && form.touched.hash}
+                        >
+                          <FormLabel>Hash</FormLabel>
+                          <Input
+                            {...field}
+                            placeholder="Ingrese el hash..."
+                            type="text"
+                            maxLength={50}
+                          />
+                          <FormErrorMessage>
+                            {form.errors.hash}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Button
+                      mt={4}
+                      bg={"blue.400"}
+                      color={"white"}
+                      _hover={{
+                        bg: "blue.500",
+                      }}
+                      // colorScheme='teal'
+                      isLoading={props.isSubmitting}
+                      type="submit"
+                    >
+                      Buscar
+                    </Button>
+                  </Form>
+                </Stack>
+              </Box>
             </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+          </Flex>
+        )}
+      </Formik>
     </>
   );
 };
