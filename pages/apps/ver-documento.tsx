346c3030210bd66589e0899ddba4206bdfad1583
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2022-11-09 02:29:57 -0300

    modified in ver-documento

--- pages/apps/ver-documento.tsx
+++ pages/apps/ver-documento.tsx
@@ -27,12 +27,27 @@ import {
   NumberInputStepper,
   FormErrorMessage,
   Container,
+  SimpleGrid,
+  StackDivider,
+  AspectRatio,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
 import axios from "axios";
 import FormData from 'form-data';
 import Swal from "sweetalert2";
 import useSessionStorage from "../../components/hooks/useSessionStorage";
 import { Router, useRouter } from "next/router";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useBreakpointValue,
+} from '@chakra-ui/react'
 
 // const { isOpen, onOpen, onClose } = useDisclosure()
 // const cancelRef = React.useRef()
@@ -43,9 +58,32 @@ const VerDocumento: React.FC = () => {
   return (
     <>
       <Navbar></Navbar>
-      <Container maxW={"4xl"}>
-        <iframe src={IPFS}></iframe>
-      </Container>
+
+      <Container maxW={'full'} py={12}>
+      <Grid
+  h='200px'
+  templateRows='repeat(2, 1fr)'
+  templateColumns='repeat(3, 1fr)'
+  gap={4}
+>
+  <GridItem colSpan={1}>
+  <Heading>UTN-FRT</Heading>
+  <Text fontSize='2xl'>Emisor:</Text>
+  <Text fontSize='lg'>Universidad Tecnologica Nacional</Text>
+    <Text fontSize='2xl'>Fecha de Emisión</Text>
+    <Text fontSize='lg'>{fechaHora}</Text>
+    <Text fontSize='2xl'>Certifica a:</Text>
+    <Text fontSize='lg'>{emailAlumno}</Text>
+    <Text fontSize='2xl'>Hash:</Text>
+    <Text fontSize='lg'>{hash}</Text>
+    </GridItem> 
+  <GridItem colSpan={2} >
+  <AspectRatio maxW='full' ratio={4 / 3}>
+  <iframe/>
+</AspectRatio>
+  </GridItem>
+</Grid>
+    </Container>
     </>
   );
 };
